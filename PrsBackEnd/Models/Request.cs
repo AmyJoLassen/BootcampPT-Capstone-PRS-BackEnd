9f421d398a92608e82bf050c63694fc7f2dd961a
AmyLassen <hatfeildaj613@gmail.com>
2023-02-18 14:31:43 -0500

    Added models (user, vendor, product)

---
+++ PrsBackEnd/Models/Request.cs
@@ -0,0 +1,10 @@
+﻿namespace PrsBackEnd.Models
+{
+    public class Request
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Justification { get; set; }
+
+    }
+}
---
+++ PrsBackEnd/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrsBackEnd.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(30)]
+        public string Username { get; set; }
+
+        [StringLength(30)]
+        public string Password { get; set; }
+
+        [StringLength(30)]
+        public string Firstname { get; set; }
+
+        [StringLength(30)]
+        public string Lastname { get; set; }
+
+        [StringLength(15)]
+        public string? Phone { get; set; }
+
+        [StringLength(255)]
+        public string? Email { get; set; }
+
+        [StringLength(1)]
+        public bool IsReviewer { get; set; }
+
+        [StringLength(1)]
+        public bool IsAdmin { get; set; }
+
+    }
+}
