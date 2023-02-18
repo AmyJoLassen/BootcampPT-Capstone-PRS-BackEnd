c9aa24ba2db1419e3006b9f2bad943637483e275
AmyLassen <hatfeildaj613@gmail.com>
2023-02-18 16:17:42 -0500

    Added Users, Vendors, and Products Controllers

---
+++ PrsBackEnd/Data/PrsDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrsBackEnd.Models;
+
+namespace PrsBackEnd.Data
+{
+    public class PrsDbContext : DbContext  // not  a poco
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Vendor> Vendors { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+
+        // constructor to support dependency injection (via a service)
+        public PrsDbContext(DbContextOptions<PrsDbContext> options) : base(options)         // constructor - public, returns nothing, same name as class
+        {
+        }
+
+    }
+}
